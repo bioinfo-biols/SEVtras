265d9a8ed7dccee24ebab1f00bb3286ccf4e0f7d
Ruiqiao He <34981680+RuiqiaoHe@users.noreply.github.com>
2023-12-02 13:08:24 +0800

    Initial

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+setup(
+    name='SEVtras',
+    version='0.2',
+    author='Ruiqiao He',
+    author_email='ruiqiaohe@gmail.com',
+    packages=['SEVtras'],
+    license="GPL",
+    url='http://pypi.python.org/pypi/SEVtras/',
+    description='sEV-containing droplet identification in scRNA-seq data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "numpy>=1.21.5",
+        "pandas>=1.1.2",
+        "scipy>=1.5.4",
+        "statsmodels>=0.12.1",
+        "anndata>=0.7.4",
+        "gseapy",
+        'umap-learn>=0.5.0',#3.10
+        # "copy",
+        # "sys",
+        # "os",
+        # "multiprocessing",
+        # "pickle>=4.0",
+        # "scanpy>=1.6.0",
+    ],
+    python_requires='>=3.7.1',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+      ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'SEVtras=SEVtras.main:SEVtras_command',
+        ]
+    }
+)
